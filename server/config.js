bb4aa70a1eb7caab2dd749dfac5b3936d586ee5b
Alan Medina <Alan.Medina@flex.com>
2017-09-08 09:47:07 -0700

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: 'http://sandbox2.flex.com.924elmp01.blackmesh.com',
+    startURL: 'http://sandbox2.flex.com.924elmp01.blackmesh.com/user',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
     // startMethod: "login.onStart",
